5f718c1be4ce863ac832d67effbd9017cbbba603
Arves777 <adrianrosselis2017@gmail.com>
2024-04-24 21:17:39 -0300

    build

---
+++ src/app/produtos/produtos.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProdutosRoutingModule } from './produtos-routing.module';
+import { ProdutosComponent } from './produtos.component';
+import { DetalhesProdutoComponent } from './detalhes-produto/detalhes-produto.component';
+import { FormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { NotificacaoService } from '../notificacao.service';
+
+
+
+@NgModule({
+  declarations: [
+    ProdutosComponent,
+    DetalhesProdutoComponent
+  ],
+  imports: [
+    CommonModule,
+    ProdutosRoutingModule,
+    FormsModule,
+    MatSnackBarModule
+  ],
+  providers:[NotificacaoService]
+})
+export class ProdutosModule { }
